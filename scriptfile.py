ed110888f5c6ec715e36563e57290e507a52baf9
TanishqGoje <74131250+TanishqGoje@users.noreply.github.com>
2025-07-20 14:08:22 +0530

    Add files via upload

---
+++ scriptfile.py
@@ -0,0 +1,13 @@
+print('this is a script file')
+print('python file extension is .py')
+a=42
+b=2435
+c=a+b
+print(c)
+print(type(c))
+X=543.67
+Y=345.354
+z=X*Y
+print(z)
+print(type(z))
+print("this ->",'<-is a seperator that seperates 2 strings',sep='&&&')
